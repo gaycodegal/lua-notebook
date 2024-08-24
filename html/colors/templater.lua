8a4231001e0c83e0e34afac63ffb71d1a53df5e4
gaycodegal <gaycodegal@github.com>
2024-08-24 11:40:46 -0400

    add read write notifications

--- html/colors/templater.lua
+++ html/colors/templater.lua
@@ -2,6 +2,7 @@ function replaceFile(template_fname, output_fname, args)
 	 local template_file = io.open(template_fname, 'r')
 	 local template = template_file:read("*all")
 	 template_file:close()
+	 print("read", template_fname)
 	 
 	 function replacer(x)
 			return args[x] or ''
@@ -11,4 +12,6 @@ function replaceFile(template_fname, output_fname, args)
 	 local output_file = io.open(output_fname, 'w')
 	 output_file:write(result)
 	 output_file:close()
+	 print("output", output_fname)
+
 end
